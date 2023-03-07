fa9ac31169a55b1f81376e2f3d9abf7459d6bb91
TordenTrykk <tordentrykk@bk.ru>
2023-03-07 18:51:06 +0300

    Create tic tac toe march 23-1.py

---
+++ Skillfactory/tic tac toe march 23/tic tac toe march 23-1.py
@@ -0,0 +1,19 @@
+def greet():
+    print("-----------------------------")
+    print("-Welcome to tic tac toe game-")
+    print("---------input X and Y------- ")
+    print("----X is horisontal line-----")
+    print("------Y is vertical line----- ")
+    print("----------------------------- ")
+greet()
+
+field = [["[ ]"] * 3 for i in range(3) ] 
+
+def show():
+    print(f"   0   1   2")
+    for i in range(3):
+        row_info = " ".join(field[i])
+        print(f"{i} {row_info}")
+
+show()
+
