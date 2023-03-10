73c0c10bfa80bdd12033e4f6c2c23790e597ce5c
TordenTrykk <tordentrykk@bk.ru>
2023-03-10 18:11:14 +0300

    Update tic tac toe march 23-1.py

--- Skillfactory/tic tac toe march 23/tic tac toe march 23-1.py
+++ Skillfactory/tic tac toe march 23/tic tac toe march 23-1.py
@@ -19,28 +19,21 @@ showfield()
 
 def askcords():
     while True:
-        cords = input("         Your move: ").split()
-        
+        cords = input("-------Your move: ").split()
         if len(cords) != 2:
-            print(" Input two cords! ")
-            continue
-        
+            print(" Input two cords ")
+            continue 
         x, y = cords
-        
         if not(x.isdigit()) or not(y.isdigit()):
-            print(" Input digits! ")
+            print(" Input digits ")
             continue
-        
         x, y = int(x), int(y)
-        
         if 0 > x or x > 2 or  0 > y or  y > 2 :
-            print(" Cords out of space! ")
+            print(" Cords out of space ")
             continue
-        
         if field[x][y] != " ":
-            print(" Field is full! ")
+            print(" Field is full ")
             continue
-        
         return x, y
             
 askcords()
@@ -54,10 +47,10 @@ def check_winner():
         for c in cord:
             symbols.append(field[c[0]][c[1]])
         if symbols == ["X", "X", "X"]:
-            print("Winner is X!!!")
+            print("----Winner is X----")
             return True
         if symbols == ["0", "0", "0"]:
-            print("Winner is 0!!!")
+            print("----Winner is 0----")
             return True
     return False
 
@@ -76,20 +69,16 @@ while True:
     count += 1
     showfield()
     if count % 2 == 1:
-        print(" X to move!")
+        print("----X to move")
     else:
-        print(" 0 to move!")
-    
+        print("----0 to move")
     x, y = askcords()
-    
     if count % 2 == 1:
         field[x][y] = "X"
     else:
         field[x][y] = "0"
-    
     if check_winner():
-        break
-    
+        break  
     if count == 9:
-        print(" Draw!")
+        print("----Draw----")
         break
